cf9b09abf88c229ed3d75497029db7c862f52ded
bagas12dwi <bag12dwi.s@gmail.com>
2022-07-11 01:35:08 +0700

    updated tentang kami

--- src/home/about.jsx
+++ src/home/about.jsx
@@ -2,21 +2,29 @@ import React from "react";
 import Header from "./layouts/header";
 import Footer from "./layouts/footer";
 
-function Content(){
-    return (
-        <section className='Content' id="about" class="about" style={{paddingTop: '150px'}}>
-                <div class="container">
-          
-                  <div class="row">
-                    <div class="col-lg-6">
-                      <img src="assets/img/HANGTUAH 7.jpg" class="img-fluid" alt=""/>
-                    </div>
-                    <div class="col-lg-6 pt-4 pt-lg-0">
-                      <h3>PROFIL SEKOLAH</h3>
-                      <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                      </p>
-                      <ul>
+function Content() {
+  return (
+    <section
+      className="Content"
+      id="about"
+      class="about"
+      style={{ paddingTop: "150px" }}
+    >
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <img src="assets/img/HANGTUAH 7.jpg" class="img-fluid" alt="" />
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0">
+            <h3>PROFIL SEKOLAH</h3>
+            <p>
+              Alamat : Jl. Golf 1 , Kel. Gunungsari, Kec. Dukuh Pakis, Kota{" "}
+              <br />
+              Surabaya Email : hangtuahtujuhyes@yahoo.com sdhttujuh@gmail.com
+              <br />
+              Telepon : 031-5664789
+            </p>
+            {/* <ul>
                         <li><i class="bx bx-check-double"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
                         <li><i class="bx bx-check-double"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
                       </ul>
@@ -31,25 +39,24 @@ function Content(){
                           <h4>Ullamco laboris nisi</h4>
                           <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
                         </div>
-                      </div>
-                    </div>
-                  </div>
-          
-                </div>
-              </section>
-    );
+                      </div> */}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-function About(){
-    return (
-        <div className="About">
-            <Header/>
-            <main id="main">
-                <Content/>
-            </main>
-        <Footer/>
-        </div>
-    );
+function About() {
+  return (
+    <div className="About">
+      <Header />
+      <main id="main">
+        <Content />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default About;
