32ea0eb3a0df76063e56152925d3eeb3589d81ee
bagas12dwi <bag12dwi.s@gmail.com>
2022-09-20 07:51:25 +0700

    updated api url

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,2 +1,2 @@
-export const urlApi = "https://sdhangtuah7sby.com/admin/wp-json/hangtuahapi";
-// export const urlApi = "http://localhost/sdhangtuah7/wp-json/hangtuahapi";
+export const urlApi = "https://sdhangtuah7sby.com/adm/wp-json/hangtuahapi";
+// export const urlApi = "localhost/sdhangtuah7/wp-json/hangtuahapi";
